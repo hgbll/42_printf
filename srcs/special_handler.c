dfb22e48733d915ddab24f4b1925b963e9eeaf71
hbally <hbally@student.42.fr>
2019-01-04 10:52:10 +0100

    remove readme, fixed formatting on special_handler.c

--- srcs/special_handler.c
+++ srcs/special_handler.c
@@ -1,17 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   special_handler.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbally <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/04 10:47:32 by hbally            #+#    #+#             */
+/*   Updated: 2019/01/04 10:48:56 by hbally           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-/*
-**	Handles zero with no precision
-**	Handles octal '#' flag with
-**	'0' flag and width > size of output or
-**	precision bigger than the amount of digits in the number
-**	(i.e. never put octal 0 flag if there's a zero after it
-**
-*/
-
-void		special_handler(const char *s,
-							const char c,
-							t_index *params)
+void		special_handler(const char *s, const char c, t_index *params)
 {
 	if (c != 's' && c != 'c' && c != 'f')
 	{
@@ -20,8 +21,8 @@ void		special_handler(const char *s,
 			if (params->flags & 0x1)
 				if ((params->precision > (long long)params->size) ||
 						(!(params->flags & 0x4) && params->flags & 0x2 &&
-						 params->precision == -1 &&
-						 width(s, c, params, 0) > 0) ||
+						params->precision == -1 &&
+						width(s, c, params, 0) > 0) ||
 						(params->precision != 0 && s[0] == '0'))
 					params->flags -= 0x1;
 		}
